060d4817ee9f78339249e02bcf7e466dbc0fdbc3
Craftsman0001 <131674449+Craftsman0001@users.noreply.github.com>
2025-11-28 16:36:48 +0100

    Created the slime enemy

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,51 +1,68 @@
 extends CharacterBody2D
 
-
-const SPEED = 150.0
+const SPEED = 180.0
 const JUMP_VELOCITY = -400.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $Marker2D/AnimatedSprite2D
 @onready var marker_2d: Marker2D = $Marker2D
+@onready var attack_zone: CollisionShape2D = $Marker2D/AnimatedSprite2D/AttackZone/CollisionShape2D
+
+var is_attacking = false
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-	# Handle attack
-
+	if is_attacking == false:
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			
+		# Handle attack
+		if Input.is_action_just_pressed("attack") and is_on_floor():
+			animated_sprite_2d.play("attack")
+			is_attacking = true
+			attack_zone.disabled = false
+			velocity.x = 0
 
 	# Get the input direction : -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
 	
 	# Flip the sprite
-	if direction > 0:
-		#animated_sprite_2d.flip_h = false
-		marker_2d.scale.x = 1
-	elif direction < 0:
-		#animated_sprite_2d.flip_h = true
-		marker_2d.scale.x = -1
+	if not is_attacking:
+		if direction > 0:
+			#animated_sprite_2d.flip_h = false
+			marker_2d.scale.x = 1
+		elif direction < 0:
+			#animated_sprite_2d.flip_h = true
+			marker_2d.scale.x = -1
 	
 	# Play animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite_2d.play("idle")
+	if is_attacking == false:
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite_2d.play("idle")
+			else:
+				animated_sprite_2d.play("run")
 		else:
-			animated_sprite_2d.play("run")
-	else:
-		if velocity.y < 0:
-			animated_sprite_2d.play("jump")
-		else:
-			animated_sprite_2d.play("fall")
+			if velocity.y < 0:
+				animated_sprite_2d.play("jump")
+			else:
+				animated_sprite_2d.play("fall")
 	
 	# Apply movement
-	if direction:
-		velocity.x = direction * SPEED
+	if not is_attacking:
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = 0
 	move_and_slide()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+		if animated_sprite_2d.animation == "attack":
+			attack_zone.disabled = true
+			is_attacking = false
---
+++ scripts/slime.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+const SPEED = 20.0
+
+@onready var ray_cast_bottom_right: RayCast2D = $RayCastBottom
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var direction = 1
+var is_dead = false
+
+func add_gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else :
+		velocity.y = 0
+
+func move_enemy() -> void:
+	if not is_dead:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = 0
+	move_and_slide()
+
+func change_direction() -> void:
+	if not ray_cast_bottom_right.is_colliding() or ray_cast_right.is_colliding():
+		direction *= -1
+		animated_sprite_2d.scale.x = direction
+		ray_cast_bottom_right.position.x *= -1
+		ray_cast_right.position.x *= -1
+		ray_cast_right.scale.x *= -1 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	add_gravity(delta)
+	move_enemy()
+	change_direction()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == "death":
+		queue_free()
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.name == "AttackZone":
+		is_dead = true
+		animated_sprite_2d.play("death")
